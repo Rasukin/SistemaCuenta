4c64d63ae31fe348cd5b62f5f0309bb1b8fe3279
franmonge <franmonge06@gmail.com>
2018-04-14 12:01:52 -0600

    Revert "Creación de vistaCrearCuenta y vistaMovimientos y sus respectivos controladores"
    This reverts commit 36ea2325cc490120a2b4b1cb728050f624cf656a.

--- SistemaCuentas/src/AccountsSystem/Conexion.java
+++ SistemaCuentas/src/AccountsSystem/Conexion.java
@@ -5,30 +5,10 @@
  */
 package AccountsSystem;
 
-
-
-import java.sql.ResultSet;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.DriverManager;
-import javax.swing.JOptionPane;
-
 /**
  *
  * @author Francisco
  */
 public class Conexion {
-    ResultSet rs;
-    private Connection conn;
     
-    public Connection Conexion(String username, String password) {
-        try {
-            Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Subasta_DB",username,password);
-            System.out.println("Opened database Subasta_DB successfully");
-        } catch (ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null, "El usuario o contraseña no se encontraron en la base de datos", "Datos no encontrados", JOptionPane.ERROR_MESSAGE);   
-        }
-        return conn;
-    }
 }
